092dec45d001649513abca55ab1459daf383e974
Kingartos <arturos.zica@gmail.com>
2017-09-07 12:30:48 -0300

    add verify solution and refactor

--- basic_solution.rb
+++ basic_solution.rb
@@ -6,19 +6,37 @@ class BasicSolution
     @max_interaction = max_interaction
   end
 
-  def run
-    loop do
-      solution = next_solution
-      break if solution?(solution) || max?
-    end
-    max? ? 'Not Found' : solution
+  def update_max(new_max = @max_interaction)
+    @max_interaction += new_max
   end
 
-  def print_solution(solution)
-    puts formated_solution(solution)
+  def run
+    solution = @structure
+    loop do
+      solution = next_solution(@structure)
+      break if solution?(solution) || max?
+    end
+    max? ? 'Not Found' : @solution = solution
+  end
+
+  def print
+    puts formated_solution(@solution)
   end
 
   def solution?(solution)
+    calc_diagonals(solution).find do |res|
+      res.last.uniq.count < solution.count
+    end.nil?
+  end
+
+  private
+
+  def calc_diagonals(solution)
+    solution.reduce(sub: [], sum: []) do |res, el|
+      res[:sum] << el + solution.index(el)
+      res[:sub] << el - solution.index(el)
+      res
+    end
   end
 
   def formated_solution(solution)
@@ -29,19 +47,17 @@ class BasicSolution
     end
   end
 
-  private
-
   def next_solution(structure)
     solution = structure.shuffle
     duplicated?(solution) ? next_solution(structure) : save_solution(solution)
   end
 
   def save_solution(solution)
-    @interaction[structure_key(solution)] = solution
+    @interactions[structure_key(solution)] = solution
   end
 
   def duplicated?(solution)
-    @interaction[structure_key(solution)].present?
+    !@interactions[structure_key(solution)].nil?
   end
 
   def structure_key(solution)
