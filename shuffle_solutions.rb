63a04f2cda2a84e89bf07199bdcc840902a34e66
Kingartos <arturos.zica@gmail.com>
2017-09-17 14:54:25 -0300

    update solution name and create support methods

---
+++ shuffle_solutions.rb
@@ -0,0 +1,118 @@
+# ShuffleSolutions.new(X).run
+class ShuffleSolutions
+  def initialize(size = 8, threads = 1)
+    @interactions = {}
+    @structure = size.times.to_a
+    @max_interaction = 1.upto(size).reduce(:*)
+    @solutions = []
+    @max_next = 10
+    @threads = threads
+  end
+
+  def statistic
+    puts "Coverage: #{percent coverage}"
+    puts "Solutions in coverage: #{percent solutions}"
+  end
+
+  def stop
+    @stop = true
+  end
+
+  def run
+    solution = @structure
+    @stop = false
+    @threads.times.each do |i|
+      Thread.new{
+        loop do
+          solution = next_solution(@structure, 0)
+          break if solution.nil?
+          (@solutions << solution; puts @solutions.count) if solution?(solution)
+          break if max? || @stop
+        end
+      }
+    end
+    puts @solutions.count
+  end
+
+  def solutions_count
+    @solutions.count
+  end
+
+  def prints
+    @solutions.each do |solution|
+      print(solution)
+      puts '____________________'
+    end.count
+  end
+
+  def solution?(solution)
+    calc_diagonals(solution).find do |res|
+      res.last.uniq.count < solution.count
+    end.nil?
+  end
+
+  private
+
+  def coverage
+    interactions_count / @max_interaction
+  end
+
+  def solutions
+    @solutions.count / interactions_count
+  end
+
+  def interactions_count
+    @interactions_count ||= @interactions.count.to_f
+  end
+
+  def percent(num = 1.0)
+    num * 100
+  end
+
+  def calc_diagonals(solution)
+    solution.reduce(sub: [], sum: []) do |res, el|
+      res[:sum] << el + solution.index(el)
+      res[:sub] << el - solution.index(el)
+      res
+    end
+  end
+
+  def formated_solution(solution)
+    solution.map do |i|
+      mat = Array.new(solution.size, '-')
+      mat[i] = 'Q'
+      mat.join(' ')
+    end
+  end
+
+  def next_solution(structure, current_next)
+    solution = structure.shuffle
+    current_next += 1
+    if duplicated?(solution)
+      next_solution(structure, current_next) if may_next?(current_next)
+    else
+      save_solution(solution)
+      solution
+    end
+  end
+
+  def save_solution(solution)
+    @interactions[structure_key(solution)] = 0
+  end
+
+  def duplicated?(solution)
+    !@interactions[structure_key(solution)].nil?
+  end
+
+  def structure_key(solution)
+    solution.join
+  end
+
+  def may_next?(current_next)
+    current_next < @max_next
+  end
+
+  def max?
+    @interactions.size >= @max_interaction
+  end
+end
